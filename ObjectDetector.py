52c0aedf1f194674efd7a069bb0e9f6111c030c9
SHUBHAMshukla <41503585+Shubhamrock428@users.noreply.github.com>
2018-10-14 03:47:39 +0530

    Update ObjectDetector.py

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -46,8 +46,7 @@ class Detector:
                 xRightTop = int(detections[0, 0, i, 5] * cols)
                 yRightTop = int(detections[0, 0, i, 6] * rows)
 
-                cv.rectangle(img, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),
-                             (0, 0, 255))
+                cv.rectangle(img, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),(0, 0, 255))
                 if class_id in classNames:
                     label = classNames[class_id] + ": " + str(confidence)
                     labelSize, baseLine = cv.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
