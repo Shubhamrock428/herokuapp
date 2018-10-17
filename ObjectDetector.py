4bbcb2e2bf0dd1e2e662029dcf23ba01faaa227a
SHUBHAMshukla <41503585+Shubhamrock428@users.noreply.github.com>
2018-10-18 03:25:08 +0530

    Update ObjectDetector.py

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -31,7 +31,7 @@ class Detector:
     def detectObject(self, imName):
         img = cv2.cvtColor(numpy.array(imName), cv2.COLOR_BGR2RGB)
         #img = cv2.imread(numpy.array["imame"], cv2.IMREAD_GRAYSCALE)
-        cv2Net.setInput(cv2.dnn.blobFromImage(img, 0.007843, (127.5, 127.5, 127.5),swapRB=True, crop=False))
+        cv2Net.setInput(cv2.dnn.blobFromImage(img, 0.007843,(1), (127.5, 127.5, 127.5),swapRB=True, crop=False))
         detections = cv2Net.forward()
         cols = img.shape[1]
         rows = img.shape[0]
