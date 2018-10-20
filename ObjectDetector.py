a21e6768d116b3b87401b61474180f5645a3a79c
SHUBHAMshukla <41503585+Shubhamrock428@users.noreply.github.com>
2018-10-20 17:36:00 +0530

    Update ObjectDetector.py

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -48,7 +48,7 @@ PATH_TO_IMAGE = os.path.join(CWD_PATH)
 
 # Number of classes the object detector can identify
 NUM_CLASSES = 89
-
+def detectobject():
 # Load the label map.
 # Label maps map indices to category names, so that when our convolution
 # network predicts `5`, we know that this corresponds to `king`.
